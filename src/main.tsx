fd79e5e7d4b14233a256afe92dd0f119eb04d139
Alekseypavlov14 <aleshapavlov9@gmail.com>
2024-07-29 14:48:44 +0300

    added routing and default styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
+import { RouterProvider } from 'react-router-dom'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { App } from './App.tsx'
+import { routing } from './app/routing/index.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={routing} />
   </StrictMode>
 )
