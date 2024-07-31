22bc9c8982748f97fd1fab36e6b3451387219c13
Alekseypavlov14 <aleshapavlov9@gmail.com>
2024-07-31 22:54:24 +0300

    add auth protection for create-transaction page

--- src/pages/CreateTransactionPage/CreateTransactionPage.tsx
+++ src/pages/CreateTransactionPage/CreateTransactionPage.tsx
@@ -1,21 +1,24 @@
 import { TransactionForm, transactionFormCreateMode } from '@/widgets/TransactionForm'
 import { createTransaction } from '@/features/transactions'
 import { StructureLayout } from '@/layouts/StructureLayout'
+import { ProtectedRoute } from '@/app/auth'
 import { Container } from '@/shared/components/Container'
 import { Page } from '@/shared/components/Page'
 import styles from './CreateTransactionPage.module.css'
 
 export function CreateTransactionPage() {
   return (
-    <Page className={styles.CreateTransactionPage}>
-      <StructureLayout>
-        <Container>
-          <TransactionForm 
-            mode={transactionFormCreateMode}
-            onSubmit={createTransaction}
-          />
-        </Container>
-      </StructureLayout>
-    </Page>
+    <ProtectedRoute>
+      <Page className={styles.CreateTransactionPage}>
+        <StructureLayout>
+          <Container>
+            <TransactionForm 
+              mode={transactionFormCreateMode}
+              onSubmit={createTransaction}
+            />
+          </Container>
+        </StructureLayout>
+      </Page>
+    </ProtectedRoute>
   )
 }
