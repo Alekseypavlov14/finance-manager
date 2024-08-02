b8a35e5d3b9e010f97357b7c95ac36f5ef53c95b
Alekseypavlov14 <aleshapavlov9@gmail.com>
2024-08-02 17:51:29 +0300

    updated date formatters

--- src/shared/utils/date-time/index.ts
+++ src/shared/utils/date-time/index.ts
@@ -2,6 +2,6 @@ import { DateFormatter } from '@oleksii-pavlov/date-time'
 
 export const dateFormatter = new DateFormatter()
 
-export const formatFullDate = dateFormatter.createFormatter('DD MMM YYYY')
-export const formatFullDateWithoutYear = dateFormatter.createFormatter('DD MMM')
+export const formatFullDate = dateFormatter.createFormatter('DD MMMM YYYY')
+export const formatFullDateWithoutYear = dateFormatter.createFormatter('DD MMMM')
 export const formatMonth = dateFormatter.createFormatter('MMM YYYY')
