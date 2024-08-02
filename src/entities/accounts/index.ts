44e3df7943c60ba55053c49b8f372a4309bd4838
Alekseypavlov14 <aleshapavlov9@gmail.com>
2024-08-02 09:12:12 +0300

    added accounts get-account-by-filters util

--- src/entities/accounts/index.ts
+++ src/entities/accounts/index.ts
@@ -1,3 +1,4 @@
 export type { AccountEntity } from './account.entity'
 
+export { getAccountByFilters } from './utils/get-account-by-filters'
 export { accountsRepository } from './accounts.repository'
