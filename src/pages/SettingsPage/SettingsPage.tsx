433324d6ccd78119a697b5fef9e72731067d0425
Alekseypavlov14 <aleshapavlov9@gmail.com>
2024-08-02 18:25:14 +0300

    added settings components

--- src/pages/SettingsPage/SettingsPage.tsx
+++ src/pages/SettingsPage/SettingsPage.tsx
@@ -1,4 +1,9 @@
-import { ProtectedRoute } from '@/app/auth'
+import { SettingsSection, SettingsSectionContent, SettingsSectionTitle } from '@/widgets/SettingsSection'
+import { faArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons'
+import { ProtectedRoute, signOut } from '@/app/auth'
+import { Button, Popconfirm } from 'antd'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Container } from '@/shared/components/Container'
 import { Header } from '@/widgets/Header'
 import { Page } from '@/shared/components/Page'
 import styles from './SettingsPage.module.css'
@@ -8,6 +13,26 @@ export function SettingsPage() {
     <ProtectedRoute>
       <Page className={styles.SettingsPage}>
         <Header />
+
+        <Container className={styles.SettingsContent}>
+          <SettingsSection>
+            <SettingsSectionTitle>Account</SettingsSectionTitle>
+
+            <SettingsSectionContent>
+              <Popconfirm
+                title='Are you sure to sign out?'
+                onConfirm={signOut}
+                okText='Yes'
+                cancelText='No'
+              >
+                <Button>
+                  <FontAwesomeIcon icon={faArrowRightFromBracket} />
+                  Sign out
+                </Button>
+              </Popconfirm>
+            </SettingsSectionContent>
+          </SettingsSection>
+        </Container>
       </Page>
     </ProtectedRoute>
   )
