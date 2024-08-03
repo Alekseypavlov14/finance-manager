c2f49caacd318aaf098bba4b542662ec0d6a9ff3
Alekseypavlov14 <aleshapavlov9@gmail.com>
2024-08-03 14:35:48 +0300

    applied use-rates hook

--- src/features/statistics/charts/currencies-balance/use-data.ts
+++ src/features/statistics/charts/currencies-balance/use-data.ts
@@ -3,25 +3,22 @@ import { useAccountTransactions } from '@/entities/transactions'
 import { CurrenciesBalanceEntry } from './data-type'
 import { useEffect, useState } from 'react'
 import { useCurrencies } from '@/entities/currency'
-import { ratesStorage } from '@/entities/rates'
+import { useRates } from '@/entities/rates'
 
 export function useCurrenciesBalanceChartData(): CurrenciesBalanceEntry[] {
   const { transactions } = useAccountTransactions()
+  const { rates } = useRates()
   const currencies = useCurrencies()
 
   const [chartData, setChartData] = useState<CurrenciesBalanceEntry[]>([])
 
   useEffect(() => {
-    ratesStorage.getRates(currencies.map(currency => currency.label)).then(rates => {
-      const chartDataEntries = getCurrenciesBalanceChartData({
-        transactions,
-        currencies,
-        rates
-      })
-
-      setChartData(chartDataEntries)
-    })
-  }, [currencies, transactions])
+    setChartData(getCurrenciesBalanceChartData({
+      transactions,
+      currencies,
+      rates
+    }))
+  }, [transactions, currencies, rates])
 
   return chartData
 }
