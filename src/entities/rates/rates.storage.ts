45ed54e113b39eb0e82e6fcf9bdbb7dea8558f45
Alekseypavlov14 <aleshapavlov9@gmail.com>
2024-08-14 23:33:10 +0300

    added rates cache

--- src/entities/rates/rates.storage.ts
+++ src/entities/rates/rates.storage.ts
@@ -1,23 +1,34 @@
 import { getRateByCurrencyCodes } from './utils/get-rate-by-currency-codes'
+import { DAY_IN_MILLISECONDS } from '@/shared/constants'
 import { USD_CURRENCY_CODE } from './constants'
+import { CollectionCache } from '@oleksii-pavlov/storages'
 import { RateEntity } from './rate.entity'
 
 export class RatesStorage {
+  private readonly cache = new CollectionCache<RateEntity, string>({
+    key: 'rates',
+    timeout: DAY_IN_MILLISECONDS,
+    selector: rate => rate.currencyCode
+  })
+
   async getRates(currencyCodes: string[]): Promise<RateEntity[]> {
-    const rates = await Promise.all(currencyCodes.map<Promise<RateEntity>>(async code => ({
-      currencyCode: code,
-      rateToUSD: await this.getRateByCurrencyCode(code)
-    })))
+    const rates = await Promise.all(currencyCodes.map<Promise<RateEntity>>(async code => {
+      const cachedRate = this.cache.getValueById(code)
+      if (cachedRate) return cachedRate
+
+      this.cache.removeValueById(code)
+
+      const rate: RateEntity = {
+        currencyCode: code,
+        rateToUSD: await this.getRateByCurrencyCode(code)
+      }
+
+      this.cache.addValue(rate)
+
+      return rate
+    }))
 
     return rates
-  }
-
-  async getRate(currencyCode: string): Promise<RateEntity> {
-    const rateValue = await this.getRateByCurrencyCode(currencyCode)
-
-    const rate: RateEntity = { currencyCode, rateToUSD: rateValue }
-
-    return rate
   }
 
   private async getRateByCurrencyCode(currencyCode: string): Promise<number> {
